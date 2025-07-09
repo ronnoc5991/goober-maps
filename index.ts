81bf84c8f50509ef69e397bb6a6f6d6787a114a9
Connor Streng <cstreng95@gmail.com>
2025-07-09 10:27:40 -0400

    feat: add TS to project

---
+++ index.ts
@@ -0,0 +1,16 @@
+// draw a circle in the middle of it
+// make sure that repositions in the center on resizes?
+
+const canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+
+const drawingContext = canvasElement.getContext("2d");
+
+function resizeCanvasToFillViewport() {
+  const { innerWidth, innerHeight } = window;
+  canvasElement.width = innerWidth;
+  canvasElement.height = innerHeight;
+}
+
+resizeCanvasToFillViewport();
+
+window.addEventListener("resize", resizeCanvasToFillViewport);
